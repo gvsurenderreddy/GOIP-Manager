45a9a741bc02fb78aba828b9fc07613bec8a62c2
Shido <Shido@Kitsune>
2014-07-01 20:46:05 +0400

    Revert "Additional admin actions."
    This reverts commit e2ee9ec1116d84843118cb3cd4ba5196de3da3e0.

--- smsbank/apps/hive/admin.py
+++ smsbank/apps/hive/admin.py
@@ -6,42 +6,24 @@ from models import (
     CallForwarding,
     Sms
 )
-from smsbank.apps.hive.client import GOIPClient, DaemonClient
+from smsbank.apps.hive.client import GOIPClient
 
 
 class DeviceAdmin(admin.ModelAdmin):
-    actions = [
-        'reboot',
-        'shutdown',
-        'terminate',
-        'restart'
-    ]
+    actions = ['reboot', 'shutdown']
 
     def reboot(self, request, queryset):
-        """Reboot selected GOIP devices"""
         for device in queryset:
             GOIPClient(device.device_id).goip_restart()
 
     reboot.short_description = u'Перезагрузить устройствa'
 
     def shutdown(self, request, queryset):
-        """Shutdown selected GOIP devices"""
         for device in queryset:
             GOIPClient(device.device_id).goip_shutdown()
 
     shutdown.short_description = u'Выключить устройства'
 
-    def terminate(self, request, queryset):
-        """Terminate daemon itself"""
-        DaemonClient().terminate()
-
-    terminate.short_description = u'Завершить демона'
-
-    def restart(self, request, queryset):
-        """Restart daemon itself"""
-        DaemonClient().restart()
-
-    restart.short_description = u'Перезапустить демона'
 
 admin.site.register(Device, DeviceAdmin)
 admin.site.register([Sms, DeviceList, CallForwarding])
