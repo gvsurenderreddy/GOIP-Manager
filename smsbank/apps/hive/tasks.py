550a60c7d0d670b568b110f6a08e52eed012702a
Artiom Basenko <demi.log@gmail.com>
2014-07-06 18:41:49 +0400

    Logger instead of print.

--- smsbank/apps/hive/tasks.py
+++ smsbank/apps/hive/tasks.py
@@ -8,7 +8,9 @@ from smsbank.apps.hive.services import new_sms, initialize_device
 
 @shared_task
 def create_sms(recipient, message, inbox, device_id):
-    print 'New SMS! [device:%s]%s:%s' % (device_id, recipient, message)
+    create_sms.get_logger().info(
+        'New SMS! [device:%s]%s:%s' % (device_id, recipient, message)
+    )
     new_sms(
         recipient,
         message,
@@ -19,5 +21,7 @@ def create_sms(recipient, message, inbox, device_id):
 
 @shared_task
 def auth_device(device_id, host, port):
-    print 'Authorizing device! [device:%s]%s:%s' % (device_id, host, port)
+    auth_device.get_logger().info(
+        'Authorizing device! [device:%s]%s:%s' % (device_id, host, port)
+    )
     initialize_device(device_id, host, port)
