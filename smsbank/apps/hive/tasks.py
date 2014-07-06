55ae9746597b00d28a59894dec1eeb0142e299ae
Artiom Basenko <demi.log@gmail.com>
2014-07-06 18:44:29 +0400

    Rollback.

--- smsbank/apps/hive/tasks.py
+++ smsbank/apps/hive/tasks.py
@@ -8,9 +8,7 @@ from smsbank.apps.hive.services import new_sms, initialize_device
 
 @shared_task
 def create_sms(recipient, message, inbox, device_id):
-    create_sms.get_logger().info(
-        'New SMS! [device:%s]%s:%s' % (device_id, recipient, message)
-    )
+    print 'New SMS! [device:%s]%s:%s' % (device_id, recipient, message)
     new_sms(
         recipient,
         message,
@@ -21,7 +19,5 @@ def create_sms(recipient, message, inbox, device_id):
 
 @shared_task
 def auth_device(device_id, host, port):
-    auth_device.get_logger().info(
-        'Authorizing device! [device:%s]%s:%s' % (device_id, host, port)
-    )
+    print 'Authorizing device! [device:%s]%s:%s' % (device_id, host, port)
     initialize_device(device_id, host, port)
